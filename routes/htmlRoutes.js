fa5a52330ee893bbf4a3a2df7c3df2584ffa61d7
Cara R <cara.rosa.02@gmail.com>
2024-01-23 11:44:15 -0600

    fixed homepage and notes routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const router = require('express').Router();
 
-module.exports = (app) => {
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-};
+// GET Route for homepage
+router.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
 
+// GET route for notes page
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+module.exports = router;
